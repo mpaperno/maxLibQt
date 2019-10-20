39baf00667c0bc6af6221dd9b7aac0976893217a
Max Paperno <max@wdg.us>
2019-10-19 23:16:39 -0400

    [AppDebugMessageHandler] New version, many new features, much safety. Docs updated.

--- src/core/AppDebugMessageHandler.cpp
+++ src/core/AppDebugMessageHandler.cpp
@@ -26,70 +26,148 @@
 
 #include "AppDebugMessageHandler.h"
 
+#include <cstdlib>
+#include <iostream>
 #include <QFileDevice>
 
-AppDebugMessageHandler::AppDebugMessageHandler(QObject *parent) :
-  QObject(parent),
-  m_appDebugOutputLevel(APP_DBG_HANDLER_DEFAULT_LEVEL),
-  m_showSourcePath(APP_DBG_HANDLER_SHOW_SRC_PATH),
-  m_showFunctionDeclarations(APP_DBG_HANDLER_SHOW_FUNCTION_DECL)
+AppDebugMessageHandler::AppDebugMessageHandler() :
+  QObject(),
+  m_defaultHandler(nullptr)
 {
-	m_srcPathFilter = QRegularExpression("^" APP_DBG_HANDLER_SRC_PATH "[\\\\\\/](.*?)$", QRegularExpression::InvertedGreedinessOption);
+	setAppDebugOutputLevel(APP_DBG_HANDLER_DEFAULT_LEVEL);
+	setShowSourcePath(APP_DBG_HANDLER_SHOW_SRC_PATH);
+	setSourceBasePath(QStringLiteral(APP_DBG_HANDLER_SRC_PATH));
+	setShowFunctionDeclarations(APP_DBG_HANDLER_SHOW_FUNCTION_DECL);
+	setShowTimestamp(APP_DBG_HANDLER_SHOW_TIMESTAMP);
+	setTimestampFormat(QStringLiteral(APP_DBG_HANDLER_TIMESTAMP_FORMAT));
+	defaultMessagePattern();  // preload
+#if (QT_VERSION < QT_VERSION_CHECK(5, 4, 0))
 	m_functionFilter = QRegularExpression("^.+?(\\w+\\().+$");
-}
-
-AppDebugMessageHandler * AppDebugMessageHandler::instance()
-{
-	static AppDebugMessageHandler * _instance = NULL;
-#if APP_DBG_HANDLER_ENABLE
-	if(_instance == NULL && !qApp->closingDown())
-		_instance = new AppDebugMessageHandler(qApp);  // Ensure this object is cleaned up when the QApplication exits.
 #endif
-	return _instance;
 }
 
-void AppDebugMessageHandler::setAppDebugOutputLevel(const quint8 &appDebugOutputLevel)
+// static
+AppDebugMessageHandler *AppDebugMessageHandler::instance()
 {
-	m_appDebugOutputLevel = appDebugOutputLevel;
+	static AppDebugMessageHandler instance;
+	return &instance;
+}
+
+void AppDebugMessageHandler::setAppDebugOutputLevel(quint8 appDebugOutputLevel)
+{
+	QWriteLocker locker(&m_mutex);
+	m_appDebugOutputLevel = qMin<quint8>(appDebugOutputLevel, 4);
 }
 
 void AppDebugMessageHandler::setShowSourcePath(bool showSourcePath)
 {
+	QWriteLocker locker(&m_mutex);
 	m_showSourcePath = showSourcePath;
+	m_defaultPattern.clear();
+}
+
+void AppDebugMessageHandler::setSourceBasePath(const QString &path)
+{
+	QWriteLocker locker(&m_mutex);
+	m_srcPathFilter = path.isEmpty() ? QRegularExpression() : QRegularExpression(QStringLiteral("^%1[\\\\\\/](.*?)$").arg(path), QRegularExpression::InvertedGreedinessOption);
 }
 
 void AppDebugMessageHandler::setShowFunctionDeclarations(bool showFunctionDeclarations)
 {
+	QWriteLocker locker(&m_mutex);
 	m_showFunctionDeclarations = showFunctionDeclarations;
+	m_defaultPattern.clear();
+}
+
+void AppDebugMessageHandler::setShowTimestamp(bool showTimestamp)
+{
+	QWriteLocker locker(&m_mutex);
+	m_showTimestamp = showTimestamp;
+	m_defaultPattern.clear();
+}
+
+void AppDebugMessageHandler::setTimestampFormat(const QString &timeFormat)
+{
+	QWriteLocker locker(&m_mutex);
+	m_tsFormat = timeFormat;
+	m_defaultPattern.clear();
+}
+
+void AppDebugMessageHandler::setMessagePattern(const QString &pattern)
+{
+	QWriteLocker locker(&m_mutex);
+	m_msgPattern = pattern;
 }
 
 void AppDebugMessageHandler::addOutputDevice(QIODevice *device)
 {
+	QWriteLocker locker(&m_mutex);
 	if (device && !m_outputDevices.contains(device))
 		m_outputDevices.append(device);
 }
 
 void AppDebugMessageHandler::removeOutputDevice(QIODevice *device)
 {
-	if (device) {
-		// no QVector::removeAll() in Qt < 5.4
-		int i = 0;
-		foreach (QIODevice * d, m_outputDevices) {
-			if (d == device)
-				m_outputDevices.remove(i);
-			++i;
-		}
+	if (!device)
+		return;
+	QWriteLocker locker(&m_mutex);
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
+	m_outputDevices.removeAll(device);
+#else
+	int i = 0;
+	foreach (QIODevice * d, m_outputDevices) {
+		if (d == device)
+			m_outputDevices.remove(i);
+		++i;
 	}
+#endif
+}
+
+QString AppDebugMessageHandler::defaultMessagePattern() const
+{
+	if (!m_defaultPattern.isEmpty())
+		return m_defaultPattern;
+
+	QString msgPattern;
+	if (m_showTimestamp)
+		msgPattern.append(QStringLiteral("[%{time %1}] ").arg(m_tsFormat));
+
+	msgPattern.append(QLatin1String("[%{short-type}] "));
+
+	if (m_showSourcePath)
+		msgPattern.append(QLatin1String("%{file}::"));
+
+	if (m_showFunctionDeclarations)
+		msgPattern.append(QLatin1String("%{full-function}"));
+	else
+		msgPattern.append(QLatin1String("%{function}()"));
+
+	msgPattern.append(QLatin1String(":%{line} -%{if-category} [%{category}]%{endif} %{message}"));
+
+#ifndef QT_NO_GLIB
+	msgPattern.append("%{if-fatal}\nBACKTRACE:\n%{backtrace depth=12 separator=\"\n\"}%{endif}");
+#endif
+
+	m_defaultPattern = msgPattern;
+	return msgPattern;
+}
+
+QString AppDebugMessageHandler::messagePattern() const
+{
+	return m_msgPattern.isEmpty() ? defaultMessagePattern() : m_msgPattern;
 }
 
 void AppDebugMessageHandler::installAppMessageHandler()
 {
+#if APP_DBG_HANDLER_ENABLE
 	m_defaultHandler = qInstallMessageHandler(g_appDebugMessageHandler);
+#else
+	qInstallMessageHandler(nullptr);
+#endif
 }
 
-void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogContext & context, const QString & msg)
+void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
 {
-	static const char symbols[] = { 'D', 'W', 'E', 'X', 'I' };  // correspond to QtMsgType enum
 	// normalize types, QtDebugMsg stays 0, QtInfoMsg becomes 1, rest are QtMsgType + 1
 	quint8 lvl = type;
 	if (type == QtInfoMsg)
@@ -97,7 +175,9 @@ void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogCon
 	else if (type > QtDebugMsg)
 		++lvl;
 
-	if (lvl < m_appDebugOutputLevel && type != QtFatalMsg)
+	QReadLocker locker(&m_mutex);
+
+	if (lvl < m_appDebugOutputLevel)
 		return;
 
 #if defined(Q_OS_LINUX) && (QT_VERSION < QT_VERSION_CHECK(5, 3, 0))
@@ -106,45 +186,46 @@ void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogCon
 		return;
 #endif
 
-	QString msgPattern = QString("[%1] ").arg(symbols[type]);
+	QString msgPattern = messagePattern();
 
-	if (m_showSourcePath) {
-		QString file = QString("%1::").arg(context.file);
+	QString file = context.file;
+	if (m_srcPathFilter.isValid())
 		file.replace(m_srcPathFilter, "\\1");
-		msgPattern.append(file);
-	}
-	if (m_showFunctionDeclarations)
-		msgPattern.append(context.function);
-	else
-		msgPattern.append("%{function}()");
 
-	msgPattern.append(":%{line} -%{if-category} [%{category}]%{endif} %{message}");
+	const bool hasDevices = !m_outputDevices.isEmpty();
 
-	if (type == QtFatalMsg)
-		msgPattern.append("\nBACKTRACE:\n%{backtrace depth=12 separator=\"\n\"}");
+	locker.unlock();
+
+	msgPattern.replace(QLatin1String("%{short-type}"), shortTypeNames().value(type, QStringLiteral("?")));
+	msgPattern.replace(QLatin1String("%{full-function}"), context.function);
+
+	QMessageLogContext newContext(qPrintable(file), context.line, context.function, context.category);
 
 	qSetMessagePattern(msgPattern);
 
-	if (!m_defaultHandler || m_outputDevices.size() || receivers(SIGNAL(messageOutput(quint8, const QString &)))) {
+	if (!m_defaultHandler || hasDevices || receivers(SIGNAL(messageOutput(quint8, const QString &)))) {
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
-		msgPattern = qFormatLogMessage(type, context, msg);
+		msgPattern = qFormatLogMessage(type, newContext, msg);
 #else
+		msgPattern.replace("%{type}", fullTypeNames().value(type, "unknown"));
 		msgPattern.replace("%{line}", QString::number(context.line));
 		msgPattern.replace("%{if-category} [%{category}]%{endif}", QString(context.category));
 		msgPattern.replace("%{message}", msg);
-		if (!m_showFunctionDeclarations) {
-			QString func = context.function;
-			msgPattern.replace("%{function}", func.replace(m_functionFilter, "\\1)"));
-		}
+		msgPattern.replace("%{function}", QString(context.function).replace(m_functionFilter, "\\1)"));
 #endif
 
 		emit messageOutput(lvl, msgPattern);
 
-		foreach (QIODevice * d, m_outputDevices) {
-			if (d && d->isWritable() && (!d->property("level").isValid() || d->property("level").toInt() <= lvl)) {
-				d->write(qPrintable(msgPattern % "\n"));
-				if (QFileDevice * fd = qobject_cast<QFileDevice *>(d))
-					fd->flush();
+		if (hasDevices) {
+			locker.relock();
+			const QVector<QIODevice *> devices(m_outputDevices);
+			locker.unlock();
+			for (QIODevice * d : devices) {
+				if (d && d->isWritable() && (!d->property("level").isValid() || d->property("level").toInt() <= lvl)) {
+					d->write(qPrintable(msgPattern + "\n"));
+					if (QFileDevice * fd = qobject_cast<QFileDevice *>(d))
+						fd->flush();
+				}
 			}
 		}
 	}
@@ -152,7 +233,7 @@ void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogCon
 	// if (QThread::currentThread() == qApp->thread())  // gui thread
 
 	if (m_defaultHandler) {
-		m_defaultHandler(type, context, msg);
+		m_defaultHandler(type, newContext, msg);
 	}
 	else {
 		fprintf(stderr, "%s", qPrintable(msgPattern));
@@ -160,6 +241,34 @@ void AppDebugMessageHandler::messageHandler(QtMsgType type, const QMessageLogCon
 			abort();
 	}
 }
+
+// static
+QHash<int, QString> &AppDebugMessageHandler::shortTypeNames()
+{
+	static QHash<int, QString> symbols({
+	  {QtDebugMsg,    tr("D")},
+	  {QtWarningMsg,  tr("W")},
+	  {QtCriticalMsg, tr("C")},
+	  {QtFatalMsg,    tr("F")},
+	  {QtInfoMsg,     tr("I")}
+	});
+	return symbols;
+}
+
+#if (QT_VERSION < QT_VERSION_CHECK(5, 4, 0))
+// static
+QHash<int, QString> &AppDebugMessageHandler::fullTypeNames()
+{
+	static QHash<int, QString> symbols({
+	  {QtDebugMsg,    tr("debug")},
+	  {QtWarningMsg,  tr("warning")},
+	  {QtCriticalMsg, tr("critical")},
+	  {QtFatalMsg,    tr("fatal")},
+	  {QtInfoMsg,     tr("info")}
+	});
+	return symbols;
+}
+#endif
 
 // Message handler which is installed using qInstallMessageHandler. This needs to be global.
 void g_appDebugMessageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg)
